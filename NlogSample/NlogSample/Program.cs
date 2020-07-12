0a3793d69f478697844e8e6a0dbb1bd64ab0f391
sak52jp <charan.49jp@gmail.com>
2020-07-13 08:22:16 +0900

    NLogサンプルを追加した

---
+++ NlogSample/NlogSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NlogSample
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            while(line != "exit")
+            {
+                logger.Info(":" + line + "\n");
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
